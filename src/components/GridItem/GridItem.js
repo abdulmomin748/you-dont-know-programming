9e7b375310ec40ecd46bd52df69b8e14ec75854a
abdul momin <mominurrahman10748@gmail.com>
2022-09-29 15:08:40 +0600

    json data updated

--- src/components/GridItem/GridItem.js
+++ src/components/GridItem/GridItem.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './GridItem.css'
 const GridItem = (props) => {
-    const {img,name,timeRequired} = props.language;
+    const {img,name,desc,timeRequired} = props.language;
     return (
         
         <div className="card">
             <img src={img} alt=""/>
             <div className="card-body">
                 <h5 className="card-title">{name}</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">{desc}</p>
                 <p>Time required: {timeRequired}</p>
                 <a className="btn btn-primary w-100" href="#">Add to list</a>
             </div>
