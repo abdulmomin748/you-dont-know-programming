6a86f65333bb84870441965df944cb166abac453
abdul momin <mominurrahman10748@gmail.com>
2022-09-29 15:30:31 +0600

    updated design

--- src/components/GridItem/GridItem.js
+++ src/components/GridItem/GridItem.js
@@ -8,9 +8,9 @@ const GridItem = (props) => {
             <img src={img} alt=""/>
             <div className="card-body">
                 <h5 className="card-title">{name}</h5>
-                <p className="card-text">{desc}</p>
+                <p className="card-text">{desc < 20 ? desc : desc.slice(0, 70)}</p>
                 <p>Time required: {timeRequired}</p>
-                <a className="btn btn-primary w-100" href="#">Add to list</a>
+                <a className="btn btn-primary w-100 add-btn" href="#">Add to list</a>
             </div>
         </div>
     );
